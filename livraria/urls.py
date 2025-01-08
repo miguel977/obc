ffd59cc6734b73c1aba062d182c2bee60a852104
miguel977 <miguel645@gmail.com>
2025-01-08 10:58:01 -0300

    criando formulário de login

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.http import HttpResponse
-from livraria.views import home
+from livraria.views import home, logout_user
 
 urlpatterns = [
-    path('', home, name = 'home')
+    path('', home, name = 'home'),
+    path('logout/', logout_user, name='logout')
 ]
--- livraria/views.py
+++ livraria/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, logout
 
 def home(request):
     return render(request, 'home.html')
+
+def logout_user(request):
+    pass
 
 
